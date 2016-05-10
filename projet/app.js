e59c5304c07bf476fc1e6e02abe8f6d8e4ce658e
xXfergalXx <fergal.bougerol@ynov.com>
2016-05-10 13:54:25 +0200

    html2jade

--- projet/app.js
+++ projet/app.js
@@ -1,30 +1,124 @@
 var http = require('http');
 var fs = require('fs');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-// Chargement du fichier index.html affiché au client
-var server = http.createServer(function(req, res) {
-    fs.readFile('./index.html', 'utf-8', function(error, content) {
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.end(content);
+var app = express();
+
+var mongoose = require('mongoose');
+
+var routes = require('./app/routes/index');
+var users = require('./app/routes/users');
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'app/views'));
+app.set('view engine', 'jade');
+
+/*
+mongoose.connect('mongodb://localhost/bdd', function (err) {
+    if (err) {
+        throw err;
+    }
+}); */
+
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
     });
 });
 
+
+
 // Chargement de socket.io
-var io = require('socket.io').listen(server);
+var io = require('socket.io').listen(3001);
 
 // Quand un client se connecte, on le note dans la console
 io.sockets.on('connection', function (socket) {
-    console.log('Un client est connecté !');
+  socket.emit('message', 'Vous êtes bien connecté !');
+  socket.broadcast.emit('message', 'Un autre client vient de se connecter !');
+
+  socket.on('message', function (message) {
+    console.log('Un client me parle ! Il me dit : ' + message);
+  }); 
+});
+
+var net = require('net');
+
+var server = net.createServer(function(socket) {
+    socket.write('Echo server\r\n');
+    socket.pipe(socket);
+});
+
+server.listen(1337, '127.0.0.1');
+
+/*
+And connect with a tcp client from the command line using netcat, the *nix 
+utility for reading and writing across tcp/udp network connections.  I've only 
+used it for debugging myself.
+$ netcat 127.0.0.1 1337
+You should see:
+> Echo server
+*/
+
+/* Or use this example tcp client written in node.js.  (Originated with 
+example code from 
+http://www.hacksparrow.com/tcp-socket-programming-in-node-js.html.) */
+
+var net = require('net');
+
+var client = new net.Socket();
+client.connect(1337, '127.0.0.1', function() {
+    console.log('Connected');
+    client.write('Hello, server! Love, Client.');
+});
+
+client.on('data', function(data) {
+    console.log('Received: ' + data);
+    
 });
 
 
-io.sockets.on('connection', function (socket) {
-    socket.emit('message', 'Vous êtes bien connecté !');
 
-    // Quand le serveur reçoit un signal de type "message" du client    
-    socket.on('message', function (message) {
-        console.log('Un client me parle ! Il me dit : ' + message);
-    });	
-});
-
-server.listen(8080);
+module.exports = app;
